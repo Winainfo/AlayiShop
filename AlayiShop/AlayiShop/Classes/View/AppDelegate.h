e0353fec7fb4fcd2a1be2f75c65fb20653170dfc
evans <368757898@qq.com>
2015-08-24 17:44:36 +0800

    8-24 搭建项目框架－－－Evans

---
+++ AlayiShop/AlayiShop/Classes/View/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlayiShop
+//
+//  Created by 吴金林 on 15/8/24.
+//  Copyright (c) 2015年 Evans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlayiShop/AlayiShop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AlayiShop
+//
+//  Created by 吴金林 on 15/8/24.
+//  Copyright (c) 2015年 Evans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
